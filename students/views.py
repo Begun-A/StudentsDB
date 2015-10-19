f37698282f890585ad0ea6b50ad0d82ac3a4cba5
Bigun <begun.aleksandr@gmail.com>
2015-10-19 13:26:13 +0300

    Replaced the url on the url tags

--- students/views.py
+++ students/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, Http404
+from django.template import loader, RequestContext
 # Create your views here.
+
+# Students views
+def students_list(request):
+    return render(request, "students/students_list.html", {})
+
+def students_add(request):
+    return HttpResponse("<h1>Student Add From</h1>")
+
+def students_edit(request, sid):
+    return HttpResponse("<h1>Edit Student {0}</h1>".format(sid))
+
+def students_delete(request, sid):
+    return HttpResponse("<h1>Delete Student {0}</h1>".format(sid))
+
+# Groups views
+def groups_list(request):
+    return HttpResponse("<h1>Groups list</h1>")
+
+def groups_add(request):
+    return HttpResponse("<h1>Group Add From</h1>")
+
+def groups_edit(request, gid):
+    return HttpResponse("<h1>Edit Group {0}</h1>".format(gid))
+
+def groups_delete(request, gid):
+    return HttpResponse("<h1>Delete Group {0}</h1>".format(gid))
+
+
+# Journal views
+
+def journal_all(request):
+    return HttpResponse("<h1>Journal all</h1>")
+
+def journal_students(request, jid):
+    return HttpResponse("<h1>Journal Student {0}</h1>".format(jid))
+
+def journal_update(request):
+    return HttpResponse("<h1>Journal Update</h1>")
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -8,14 +8,31 @@ Function views
     2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home'
 Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns('students.views',
     url(r'^admin/', include(admin.site.urls)),
-]
+
+    # Students ,
+    url(r'^$', 'students_list', name='home'),
+    url(r'^students/add/$', 'students_add', name='students_add'),
+    url(r'^students/(?P<sid>\d+)/edit/$','students_edit', name='students_edit' ),
+    url(r'^students/(?P<sid>\d+)/delete/$','students_delete', name='students_delete'),
+
+    # Groups URLs
+    url(r'^groups$', 'groups_list', name='groups_list'),
+    url(r'^groups/add/$', 'groups_add', name='groups_add'),
+    url(r'^groups/(?P<sid>\d+)/edit/$','groups_edit', name='groups_edit'),
+    url(r'^groups/(?P<sid>\d+)/delete/$','groups_delete', name='groups_delete'),
+
+    # Journal URLs
+    url(r'^journal$', 'journal_all', name='journal_all'),
+    url(r'^journal/(?P<jid>\d+)$','journal_students', name='journal_students'),
+    url(r'^journal/update/$','journal_update', name='journal_update'),
+)
